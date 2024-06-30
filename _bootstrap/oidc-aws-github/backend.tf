b76f85f02ea1de2ce24959f6757a24c54b136061
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-06-30 10:09:27 -0700

    State migration

---
+++ _bootstrap/oidc-aws-github/backend.tf
@@ -0,0 +1,10 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  backend "s3" {
+    bucket  = "runme-terramate-aws-tfstate2"
+    encrypt = true
+    key     = "terraform/stacks/by-id/7d22b8e6-0e02-4815-995b-1cd34c826c95/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
---
+++ _bootstrap/terraform-state-bucket/backend.tf
@@ -0,0 +1,10 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  backend "s3" {
+    bucket  = "runme-terramate-aws-tfstate2"
+    encrypt = true
+    key     = "terraform/stacks/by-id/ccba15ea-e358-4f69-81dd-8da874baaa4a/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
